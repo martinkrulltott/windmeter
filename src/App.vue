0a084ffeca64859879f31497408c2d6193e75bb8
Martin Ohlson <martin@moid.se>
2019-05-30 16:38:48 +0200

    Added new font

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,20 @@ export default {
 </script>
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap');
+ 
   body {
-    font-family: sans-serif;
+    font-family: 'Nunito Sans', sans-serif;
+    font-size: 15px;
+    font-weight: 400;
+    color: #777777;
   }
+  h1, h2, h3 {
+    font-weight: 600;
+    color: #303133;
+  }
+  h2 {
+    font-size: 29px;
+  }
+
 </style>
